mloughran/signature

commit a8f9c92c8ae4423733f259429e5ea8e13438df38

Perform constant time string comparison when validating signatures
String#== is not safe for the purposes of validating crytographic
signatures because it enables timing attacks.

lib/signature.rb
@@ -213,12 +213,18 @@ module Signature
       end
 
       def validate_signature!(token)
-        unless @auth_hash["auth_signature"] == signature(token)
+        unless identical? @auth_hash["auth_signature"], signature(token)
           raise AuthenticationError, "Invalid signature: you should have "\
             "sent HmacSHA256Hex(#{string_to_sign.inspect}, your_secret_key)"\
             ", but you sent #{@auth_hash["auth_signature"].inspect}"
         end
         return true
+      end
+
+      # Constant time string comparison
+      def identical?(a, b)
+        return false unless a.bytesize == b.bytesize
+        a.bytes.zip(b.bytes).reduce(0) { |memo, (a, b)| memo += a ^ b } == 0
       end
   end
 end

commit 3d9a0bd84de4c1655fecd5ef3cff7bc220f3e7ca

Deal gracefully with nil parameters in comparison
Regression introduced in a8f9c92

lib/signature.rb
@@ -223,6 +223,8 @@ module Signature
 
       # Constant time string comparison
       def identical?(a, b)
+        return true if a.nil? && b.nil?
+        return false if a.nil? || b.nil?
         return false unless a.bytesize == b.bytesize
         a.bytes.zip(b.bytes).reduce(0) { |memo, (a, b)| memo += a ^ b } == 0
       end